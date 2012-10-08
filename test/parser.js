e9a1d023e89a14b419bed22bc432395daf137771
Harri Siirak <harri.siirak@gmail.com>
2012-10-08 17:43:46 +0300

    Cron time interval parser

---
+++ test/parser.js
@@ -0,0 +1,16 @@
+var test = require('tap').test;
+var CronInterval = require('../lib/interval');
+
+//test('parse primary interval', function(t) {
+  CronInterval.parse('2-59/5 1,10-20/2,10-20/4,22 11-26 1-6 *', function(err, interval) {
+  	//t.end();
+  });
+//});
+
+/*
+test('parse primary interval 2', function(t) {
+  CronInterval.parse('20,30,40 * * * mon,fri', function(err, interval) {
+  	t.end();
+  });
+});
+*/
