24a86378d629cf23d11d6bc717601301c634f784
Harri Siirak <harri.siirak@gmail.com>
2012-10-24 16:09:59 +0300

    Parser test updats

--- test/parser.js
+++ test/parser.js
@@ -5,12 +5,26 @@ var CronParser = require('../lib/parser');
 // Globals
 
 test('load crontab file', function(t) {
-  CronParser.parseFile('./crontab.example', function(result) {
-  	//t.ifError(err, 'Interval parse error');
-  	//t.ok(interval, 'Interval parsed');
+  CronParser.parseFile('./crontab.example', function(err, result) {
+  	t.ifError(err, 'File read error');
+  	t.ok(result, 'Crontab parsed parsed');
 
-    console.log(util.inspect(result, false, 100));
-    console.log(result.expressions[0].next());
+    t.equal(Object.keys(result.variables).length, 2, 'variables length matches');
+    t.equal(Object.keys(result.errors).length, 0, 'errors length matches');
+    t.equal(result.expressions.length, 3, 'expressions length matches');
+
+    // Parse expressions
+    var next = null;
+
+    next = result.expressions[0].next();
+    t.ok(next, 'first date');
+
+    next = result.expressions[1].next();
+    t.ok(next, 'second date');
+
+    next = result.expressions[2].next();
+    t.ok(next, 'third date');
+
   	t.end();
   });
 });
