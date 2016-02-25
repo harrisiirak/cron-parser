3809a18dc4b8c6c669fa83f00ba043aaea3ae12a
Santiago Gimeno <santiago.gimeno@gmail.com>
2016-02-26 00:17:36 +0100

    DST transitions working

--- test/timezone.js
+++ test/timezone.js
@@ -8,11 +8,58 @@ test('It works on DST start', function(t) {
       tz: 'Europe/Athens'
     };
 
-    var interval = CronExpression.parse('0 * * * *', options);
+    var interval, date;
+
+    interval = CronExpression.parse('0 * * * *', options);
     t.ok(interval, 'Interval parsed');
 
-    var date = interval.next();
+    date = interval.next();
+    t.equal(date.getMinutes(), 0, '0 Minutes');
     t.equal(date.getHours(), 4, 'Due to DST start in Athens, 3 is skipped');
+    t.equal(date.getDate(), 27, 'on the 27th');
+  
+    date = interval.next();
+    t.equal(date.getMinutes(), 0, '0 Minutes');
+    t.equal(date.getHours(), 5, '5 AM');
+    t.equal(date.getDate(), 27, 'on the 27th');
+
+    interval = CronExpression.parse('0 3 * * *', options);
+    t.ok(interval, 'Interval parsed');
+
+    date = interval.next();
+    t.equal(date.getMinutes(), 0, '0 Minutes');
+    t.equal(date.getHours(), 4, 'Due to DST start in Athens, 3 is skipped');
+    t.equal(date.getDate(), 27, 'on the 27th');
+
+    date = interval.next();
+    t.equal(date.getMinutes(), 0, '0 Minutes');
+    t.equal(date.getHours(), 3, '3 on the 28th');
+    t.equal(date.getDate(), 28, 'on the 28th');
+
+    interval = CronExpression.parse('*/20 3 * * *', options);
+    t.ok(interval, 'Interval parsed');
+
+    date = interval.next();
+    t.equal(date.getMinutes(), 0, '0 Minutes');
+    t.equal(date.getHours(), 4, 'Due to DST start in Athens, 3 is skipped');
+    t.equal(date.getDate(), 27, 'on the 27th');
+
+    date = interval.next();
+    t.equal(date.getMinutes(), 20, '20 Minutes');
+    t.equal(date.getHours(), 4, 'Due to DST start in Athens, 3 is skipped');
+    t.equal(date.getDate(), 27, 'on the 27th');
+
+    date = interval.next();
+    t.equal(date.getMinutes(), 40, '20 Minutes');
+    t.equal(date.getHours(), 4, 'Due to DST start in Athens, 3 is skipped');
+    t.equal(date.getDate(), 27, 'on the 27th');
+
+    date = interval.next();
+    t.equal(date.getMinutes(), 0, '0 Minutes');
+    t.equal(date.getHours(), 3, '3 AM');
+    t.equal(date.getDate(), 28, 'on the 27th');
+    
+
   } catch (err) {
     t.ifError(err, 'Interval parse error');
   }
@@ -27,16 +74,58 @@ test('It works on DST end', function(t) {
       tz: 'Europe/Athens'
     };
 
-    var interval = CronExpression.parse('0 * * * *', options);
+    var interval, date;
+
+    interval = CronExpression.parse('0 * * * *', options);
     t.ok(interval, 'Interval parsed');
 
-    var date = interval.next();
+    date = interval.next();
     t.equal(date.getHours(), 3, '3 AM');
+    t.equal(date.getDate(), 30, '30th');
+
     date = interval.next();
     t.equal(date.getHours(), 3, 'Due to DST end in Athens (4-->3)');
+    t.equal(date.getDate(), 30, '30th');
+
     date = interval.next();
     t.equal(date.getHours(), 4, '4 AM');
-  } catch (err) {
+    t.equal(date.getDate(), 30, '30th');
+
+    interval = CronExpression.parse('0 3 * * *', options);
+    t.ok(interval, 'Interval parsed');
+
+    date = interval.next();
+    t.equal(date.getHours(), 3, '3 AM');
+    t.equal(date.getDate(), 30, '30th');
+
+    date = interval.next();
+    t.equal(date.getHours(), 3, '3 AM');
+    t.equal(date.getDate(), 31, '31st');
+
+    interval = CronExpression.parse('*/20 3 * * *', options);
+    t.ok(interval, 'Interval parsed');
+
+    date = interval.next();
+    t.equal(date.getMinutes(), 0, '0');
+    t.equal(date.getHours(), 3, '3 AM');
+    t.equal(date.getDate(), 30, '30th');
+
+    date = interval.next();
+    t.equal(date.getMinutes(), 20, '20');
+    t.equal(date.getHours(), 3, '3 AM');
+    t.equal(date.getDate(), 30, '30th');
+
+    date = interval.next();
+    t.equal(date.getMinutes(), 40, '40');
+    t.equal(date.getHours(), 3, '3 AM');
+    t.equal(date.getDate(), 30, '30th');
+
+    date = interval.next();
+    t.equal(date.getMinutes(), 0, '0');
+    t.equal(date.getHours(), 3, '3 AM');
+    t.equal(date.getDate(), 31, '31st');
+    
+   } catch (err) {
     t.ifError(err, 'Interval parse error');
   }
 
