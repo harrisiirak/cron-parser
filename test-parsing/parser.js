5e28b3bae189fe15dbc55fc7ec790027291537f5
Emil Atanasov <heitara@gmail.com>
2020-05-19 00:43:04 +0300

    extend dayOfMonth support

--- test-parsing/parser.js
+++ test-parsing/parser.js
@@ -2,7 +2,7 @@ var util = require('util');
 var test = require('tap').test;
 var CronParser = require('../lib/parser');
 
-test('parse cron with last day in month', function(t) {
+test('parse cron with last day in a month', function(t) {
   var options = {
     currentDate: new Date(2014, 0, 1),
     endDate: new Date(2014, 10, 1)
@@ -11,16 +11,9 @@ test('parse cron with last day in month', function(t) {
   try {
     var interval = CronParser.parseExpression('0 0 L * *', options);
     t.equal(interval.hasNext(), true);
-    // t.equal(interval.hasNext(), !true);
 
-    // for (i = 0; i < 24; ++i) {
-    //   var next = interval.next();
-    //   console.log(next.toString());
-    //   t.ok(next, 'first date');
-    // }
     for (i = 0; i < 10; ++i) {
       var next = interval.next();
-      console.log(next.toString());
       t.ok(next, 'has a date');
     }
 
@@ -31,19 +24,53 @@ test('parse cron with last day in month', function(t) {
   t.end();
 });
 
-// test('parse cron with last day in month for fixed month', function(t) {
-//   var options = {
-//     currentDate: new Date(2014, 0, 1),
-//     endDate: new Date(2014, 0, 1)
-//   };
+test('parse cron with last day in feb', function(t) {
+  var options = {
+    currentDate: new Date(2016, 0, 1),
+    endDate: new Date(2016, 10, 1)
+  };
 
-//   try {
-//     var interval = CronParser.parseExpression('* * L 2 *', options);
-//     t.equal(interval.hasNext(), false);
-//   } catch (err) {
-//     t.ifError(err, 'Parse read error');
-//   }
+  try {
+    var interval = CronParser.parseExpression('0 0 1,3,6-10,L 2 *', options);
+    t.equal(interval.hasNext(), true);
+    var next = null;
+    while(interval.hasNext()) {
+      next = interval.next();
+      // console.log(next.toString());
+      t.ok(next, 'has a date');
+    }
+    //leap year
+    t.equal(next.getDate(), 29);
 
-//   t.end();
-// });
+  } catch (err) {
+    t.ifError(err, 'Parse read error');
+  }
+
+  t.end();
+});
+
+test('parse cron with last day in feb', function(t) {
+  var options = {
+    currentDate: new Date(2014, 0, 1),
+    endDate: new Date(2014, 10, 1)
+  };
+
+  try {
+    var interval = CronParser.parseExpression('0 0 1,3,6-10,L 2 *', options);
+    t.equal(interval.hasNext(), true);
+    var next = null;
+    while(interval.hasNext()) {
+      next = interval.next();
+      // console.log(next.toString());
+      t.ok(next, 'has a date');
+    }
+    //common year
+    t.equal(next.getDate(), 28);
+
+  } catch (err) {
+    t.ifError(err, 'Parse read error');
+  }
+
+  t.end();
+});
 
