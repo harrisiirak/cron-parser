8f7276d38b7929c75679e4a2c615931a58548748
JeppeLarsen <jeppe@kaarelarsen.dk>
2020-10-17 13:40:36 +0200

    Updated notes regarding tests that do not work.

--- test/timezone.js
+++ test/timezone.js
@@ -1,8 +1,7 @@
 var test = require('tap').test;
 var CronExpression = require('../lib/expression');
 
-// NOTE: These tests do not work after migrating from Moment.js to Day.js
-
+// NOTE: This test does not work after migrating from Moment.js to Day.js
 test('It works on DST start', function(t) {
   try {
     var options = {
@@ -131,6 +130,7 @@ test('It works on DST start', function(t) {
   t.end();
 });
 
+// NOTE: This test does not work after migrating from Moment.js to Day.js
 test('It works on DST end', function(t) {
   try {
     var options = {
