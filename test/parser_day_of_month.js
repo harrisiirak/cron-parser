1f0c12c57db8a3ca3be5e2149d7096975ef5c5d0
Alberto Restifo <alberto@restifo.dev>
2021-09-28 14:53:58 +0200

    Implement "L" in weekday

--- test/parser_day_of_month.js
+++ test/parser_day_of_month.js
@@ -1,11 +1,10 @@
-var util = require('util');
 var test = require('tap').test;
 var CronParser = require('../lib/parser');
 
-test('parse cron with last day in a month', function(t) {
+test('parse cron with last day in a month', function (t) {
   var options = {
     currentDate: new Date(2014, 0, 1),
-    endDate: new Date(2014, 10, 1)
+    endDate: new Date(2014, 10, 1),
   };
 
   try {
@@ -16,7 +15,6 @@ test('parse cron with last day in a month', function(t) {
       var next = interval.next();
       t.ok(next, 'has a date');
     }
-
   } catch (err) {
     t.error(err, 'Parse read error');
   }
@@ -24,10 +22,10 @@ test('parse cron with last day in a month', function(t) {
   t.end();
 });
 
-test('parse cron with last day in feb', function(t) {
+test('parse cron with last day in feb', function (t) {
   var options = {
     currentDate: new Date(2016, 0, 1),
-    endDate: new Date(2016, 10, 1)
+    endDate: new Date(2016, 10, 1),
   };
 
   try {
@@ -36,7 +34,7 @@ test('parse cron with last day in feb', function(t) {
     var next = null;
     var items = 9;
     var i = 0;
-    while(interval.hasNext()) {
+    while (interval.hasNext()) {
       next = interval.next();
       i += 1;
       t.ok(next, 'has a date');
@@ -44,7 +42,6 @@ test('parse cron with last day in feb', function(t) {
     //leap year
     t.equal(next.getDate(), 29);
     t.equal(i, items);
-
   } catch (err) {
     t.error(err, 'Parse read error');
   }
@@ -52,23 +49,22 @@ test('parse cron with last day in feb', function(t) {
   t.end();
 });
 
-test('parse cron with last day in feb', function(t) {
+test('parse cron with last day in feb', function (t) {
   var options = {
     currentDate: new Date(2014, 0, 1),
-    endDate: new Date(2014, 10, 1)
+    endDate: new Date(2014, 10, 1),
   };
 
   try {
     var interval = CronParser.parseExpression('0 0 1,3,6-10,L 2 *', options);
     t.equal(interval.hasNext(), true);
     var next = null;
-    while(interval.hasNext()) {
+    while (interval.hasNext()) {
       next = interval.next();
       t.ok(next, 'has a date');
     }
     //common year
     t.equal(next.getDate(), 28);
-
   } catch (err) {
     t.error(err, 'Parse read error');
   }
@@ -76,3 +72,31 @@ test('parse cron with last day in feb', function(t) {
   t.end();
 });
 
+test('parse cron with last monday of the month', function (t) {
+  var options = {
+    currentDate: new Date(2021, 8, 1),
+    endDate: new Date(2021, 11, 1),
+  };
+
+  try {
+    var interval = CronParser.parseExpression('0 0 0 * * 1L');
+
+    t.equal(interval.hasNext(), true);
+    
+    var next = interval.next();
+
+    t.equal(next.getDate(), 27);
+  } catch (err) {
+    t.error(err, 'Parse read error');
+  }
+
+  t.end();
+});
+
+test('fails to parse for invalid last weekday of month expression', function (t) {
+  t.throws(function() {
+    CronParser.parseExpression('0 0 0 * * L');
+  });
+
+  t.end();
+});
