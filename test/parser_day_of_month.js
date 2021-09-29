92d688eaf98b21cde96769288a6b50e6c7ca7542
Alberto Restifo <alberto@restifo.dev>
2021-09-29 12:50:03 +0200

    Improve implementation and add more tests

--- test/parser_day_of_month.js
+++ test/parser_day_of_month.js
@@ -72,20 +72,85 @@ test('parse cron with last day in feb', function (t) {
   t.end();
 });
 
-test('parse cron with last monday of the month', function (t) {
+test('parse cron with last weekday of the month', function (t) {
+  var options = {
+    currentDate: new Date(2021, 8, 1),
+    endDate: new Date(2021, 11, 1),
+  };
+
+  var testCases = [
+    { expression: '0 0 0 * * 1L', expectedDate: 27 },
+    { expression: '0 0 0 * * 2L', expectedDate: 28 },
+    { expression: '0 0 0 * * 3L', expectedDate: 29 },
+    { expression: '0 0 0 * * 4L', expectedDate: 30 },
+    { expression: '0 0 0 * * 5L', expectedDate: 24 },
+    { expression: '0 0 0 * * 6L', expectedDate: 25 },
+    { expression: '0 0 0 * * 0L', expectedDate: 26 },
+  ];
+
+  testCases.forEach(function({ expression, expectedDate }) {
+    t.test(expression, function (t) {
+      try {
+        var interval = CronParser.parseExpression(expression, options);
+
+        t.equal(interval.hasNext(), true);
+
+        var next = interval.next();
+
+        t.equal(next.getDate(), expectedDate);
+      } catch (err) {
+        t.error(err, 'Parse read error');
+      }
+
+      t.end();
+    });
+  });
+
+  t.end();
+});
+
+test('parses expression that runs on both last monday and friday of the month', function (t) {
   var options = {
     currentDate: new Date(2021, 8, 1),
     endDate: new Date(2021, 11, 1),
   };
 
   try {
-    var interval = CronParser.parseExpression('0 0 0 * * 1L');
+    var interval = CronParser.parseExpression('0 0 0 * * 1L,5L', options);
 
-    t.equal(interval.hasNext(), true);
-    
-    var next = interval.next();
+    t.equal(interval.next().getDate(), 24);
+    t.equal(interval.next().getDate(), 27);
+  } catch (err) {
+    t.error(err, 'Parse read error');
+  }
 
-    t.equal(next.getDate(), 27);
+  t.end();
+});
+
+test('parses expression that runs on both every monday and last friday of mont', function (t) {
+  var options = {
+    currentDate: new Date(2021, 8, 1),
+    endDate: new Date(2021, 8, 30),
+  };
+
+  try {
+    var interval = CronParser.parseExpression('0 0 0 * * 1,5L', options);
+
+    var dates = [];
+
+    while (true) {
+      try {
+        dates.push(interval.next().getDate());
+      } catch (e) {
+        if (e.message !== 'Out of the timespan range') {
+          throw e;
+        }
+
+        break;
+      }
+    }
+
+    t.same(dates, [6, 13, 20, 24, 27]);
   } catch (err) {
     t.error(err, 'Parse read error');
   }
@@ -95,7 +160,8 @@ test('parse cron with last monday of the month', function (t) {
 
 test('fails to parse for invalid last weekday of month expression', function (t) {
   t.throws(function() {
-    CronParser.parseExpression('0 0 0 * * L');
+    var interval = CronParser.parseExpression('0 0 0 * * L');
+    interval.next();
   });
 
   t.end();
