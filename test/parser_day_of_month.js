44f734084dd86eaae44bc2e22cec2aa48cebb008
Alberto Restifo <alberto@restifo.dev>
2021-10-11 12:18:52 +0200

    Style fixes

--- test/parser_day_of_month.js
+++ test/parser_day_of_month.js
@@ -1,10 +1,10 @@
 var test = require('tap').test;
 var CronParser = require('../lib/parser');
 
-test('parse cron with last day in a month', function (t) {
+test('parse cron with last day in a month', function(t) {
   var options = {
     currentDate: new Date(2014, 0, 1),
-    endDate: new Date(2014, 10, 1),
+    endDate: new Date(2014, 10, 1)
   };
 
   try {
@@ -22,10 +22,10 @@ test('parse cron with last day in a month', function (t) {
   t.end();
 });
 
-test('parse cron with last day in feb', function (t) {
+test('parse cron with last day in feb', function(t) {
   var options = {
     currentDate: new Date(2016, 0, 1),
-    endDate: new Date(2016, 10, 1),
+    endDate: new Date(2016, 10, 1)
   };
 
   try {
@@ -34,7 +34,7 @@ test('parse cron with last day in feb', function (t) {
     var next = null;
     var items = 9;
     var i = 0;
-    while (interval.hasNext()) {
+    while(interval.hasNext()) {
       next = interval.next();
       i += 1;
       t.ok(next, 'has a date');
@@ -49,17 +49,17 @@ test('parse cron with last day in feb', function (t) {
   t.end();
 });
 
-test('parse cron with last day in feb', function (t) {
+test('parse cron with last day in feb', function(t) {
   var options = {
     currentDate: new Date(2014, 0, 1),
-    endDate: new Date(2014, 10, 1),
+    endDate: new Date(2014, 10, 1)
   };
 
   try {
     var interval = CronParser.parseExpression('0 0 1,3,6-10,L 2 *', options);
     t.equal(interval.hasNext(), true);
     var next = null;
-    while (interval.hasNext()) {
+    while(interval.hasNext()) {
       next = interval.next();
       t.ok(next, 'has a date');
     }
@@ -72,10 +72,10 @@ test('parse cron with last day in feb', function (t) {
   t.end();
 });
 
-test('parse cron with last weekday of the month', function (t) {
+test('parse cron with last weekday of the month', function(t) {
   var options = {
     currentDate: new Date(2021, 8, 1),
-    endDate: new Date(2021, 11, 1),
+    endDate: new Date(2021, 11, 1)
   };
 
   var testCases = [
@@ -85,11 +85,11 @@ test('parse cron with last weekday of the month', function (t) {
     { expression: '0 0 0 * * 4L', expectedDate: 30 },
     { expression: '0 0 0 * * 5L', expectedDate: 24 },
     { expression: '0 0 0 * * 6L', expectedDate: 25 },
-    { expression: '0 0 0 * * 0L', expectedDate: 26 },
+    { expression: '0 0 0 * * 0L', expectedDate: 26 }
   ];
 
   testCases.forEach(function({ expression, expectedDate }) {
-    t.test(expression, function (t) {
+    t.test(expression, function(t) {
       try {
         var interval = CronParser.parseExpression(expression, options);
 
@@ -109,10 +109,10 @@ test('parse cron with last weekday of the month', function (t) {
   t.end();
 });
 
-test('parses expression that runs on both last monday and friday of the month', function (t) {
+test('parses expression that runs on both last monday and friday of the month', function(t) {
   var options = {
     currentDate: new Date(2021, 8, 1),
-    endDate: new Date(2021, 11, 1),
+    endDate: new Date(2021, 11, 1)
   };
 
   try {
@@ -127,10 +127,10 @@ test('parses expression that runs on both last monday and friday of the month', 
   t.end();
 });
 
-test('parses expression that runs on both every monday and last friday of mont', function (t) {
+test('parses expression that runs on both every monday and last friday of mont', function(t) {
   var options = {
     currentDate: new Date(2021, 8, 1),
-    endDate: new Date(2021, 8, 30),
+    endDate: new Date(2021, 8, 30)
   };
 
   try {
@@ -138,7 +138,7 @@ test('parses expression that runs on both every monday and last friday of mont',
 
     var dates = [];
 
-    while (true) {
+    while(true) {
       try {
         dates.push(interval.next().getDate());
       } catch (e) {
@@ -158,7 +158,7 @@ test('parses expression that runs on both every monday and last friday of mont',
   t.end();
 });
 
-test('fails to parse for invalid last weekday of month expression', function (t) {
+test('fails to parse for invalid last weekday of month expression', function(t) {
   t.throws(function() {
     var interval = CronParser.parseExpression('0 0 0 * * L');
     interval.next();
