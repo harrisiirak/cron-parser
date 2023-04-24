730921a5c6a794936847086d206dc1c6361f45d2
Michael Hobbs <michael.lee.hobbs@gmail.com>
2023-04-24 07:34:10 -0400

    wip - looks like we still need the index.cjs.js to support cjs even with the separate builds

---
+++ src/index.cjs.js
@@ -0,0 +1,10 @@
+const { CronConstants } = require('./CronConstants');
+const { CronFields } = require('./CronFields');
+const { CronParser } = require('./CronParser');
+const { CronExpression } = require('./CronExpression');
+
+module.exports.CronConstants = CronConstants;
+module.exports.CronFields = CronFields;
+module.exports.CronParser = CronParser;
+module.exports.CronExpression = CronExpression;
+module.exports = CronParser;
