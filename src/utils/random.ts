2e3c256b4167179aff979ba76e6aa4382f628039
mydih <8671100+MYDIH@users.noreply.github.com>
2025-04-30 16:12:25 +0200

    Add some JSDoc to utils/random.ts

--- src/utils/random.ts
+++ src/utils/random.ts
@@ -1,3 +1,13 @@
+/**
+ * A type representing a Pseudorandom Number Generator, similar to Math.random()
+ */
+export type PRNG = () => number;
+
+/**
+ * Computes a numeric hash from a given string
+ * @param {string} str A value to hash
+ * @returns {number} A numeric hash computed from the given value
+ */
 function xfnv1a(str: string) {
   let h = 2166136261 >>> 0;
   for (let i = 0; i < str.length; i++) {
@@ -7,6 +17,11 @@ function xfnv1a(str: string) {
   return () => h >>> 0;
 }
 
+/**
+ * Initialize a new PRNG using a given seed
+ * @param {number} seed The seed used to initialize the PRNG
+ * @returns {PRNG} A random number generator
+ */
 function mulberry32(seed: number) {
   return () => {
     let t = (seed += 0x6d2b79f5);
@@ -16,7 +31,11 @@ function mulberry32(seed: number) {
   };
 }
 
-export type PRNG = () => number;
+/**
+ * Generates a PRNG using a given seed. When not provided, the seed is randomly generated
+ * @param {string} str A string to derive the seed from
+ * @returns {PRNG} A random number generator correctly seeded
+ */
 export function seededRandom(str?: string): PRNG {
   const seed = str ? xfnv1a(str)() : Math.floor(Math.random() * 10_000_000_000);
   return mulberry32(seed);
