d95f09beb0851e8080259ad7dc635d2ea0336d2b
Harri Siirak <harri@siirak.ee>
2026-07-24 21:16:38 +0300

    chore: switch tsconfig to nodenext module resolution and remove ignoreDeprecations

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,9 @@ import { CronExpressionParser } from './CronExpressionParser';
 
 export { CronDate } from './CronDate';
 export { CronFieldCollection } from './CronFieldCollection';
-export { CronExpression, CronExpressionOptions } from './CronExpression';
+export { CronExpression, type CronExpressionOptions } from './CronExpression';
 export { CronExpressionParser } from './CronExpressionParser';
-export { CronFileParser, CronFileParserResult } from './CronFileParser';
+export { CronFileParser, type CronFileParserResult } from './CronFileParser';
 
 export * from './fields';
 export default CronExpressionParser;
